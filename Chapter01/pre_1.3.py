48cfb1ae4bd611181bd9d69312dffc6350d8304b
k-hirade <keisi.hirade.97@gmail.com>
2024-12-10 13:35:56 +0900

    complete 1.3 pre

--- Chapter01/pre_1.3.py
+++ Chapter01/pre_1.3.py
@@ -1,14 +1,23 @@
 import unittest
 
 def urlify(url, length):
-    new_string = ""
-    for s in url:
-        if s == "":
-            add_s = "%20"
-        new_string = "" + add_s
+    new_string = []
+    for i in range(length):
+        if url[i] == " ":
+            new_string.extend(['%', '2', '0'])
+        else:
+            new_string.append(url[i])
+    return new_string
 
-class Test(unittest.testCase):
-    data = ""
+class Test(unittest.TestCase):
+    data = [(list("Mr John Smith    "), 13, list("Mr%20John%20Smith")), 
+            (list("I like juice    "), 12, list("I%20like%20juice")), 
+            (list('much ado about nothing      '), 22, list('much%20ado%20about%20nothing'))]
 
     def test_urlify(self):
-        return True
+        for [test_string, length, answer] in self.data:
+            actual = urlify(test_string, length)
+            self.assertEqual(actual, answer)
+
+if __name__ == "__main__":
+    unittest.main()
